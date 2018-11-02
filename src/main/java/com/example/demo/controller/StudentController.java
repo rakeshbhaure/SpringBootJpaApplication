282b5dc2bc0f35b964d83920f87c2ee8743f2812
rakeshbhaure <rakeshkumarsakshi@gmail.com>
2018-11-02 17:35:01 +0530

    hello

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.domain.Student;
 import com.example.demo.service.StudentService;
@@ -19,9 +22,23 @@ public class StudentController {
 	@GetMapping("/list")
 	public String getStudentList(Model model) {
 
-		List<Student> studentList = service.studentpersist();
+		List<Student> studentList = service.getStudentList();
 		model.addAttribute("students", studentList);
 		return "studentpage";
+	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+		Student s = new Student();
+		model.addAttribute("student", s);
+		return "register";
+
+	}
+
+	@RequestMapping(value = "/processForm", method = RequestMethod.POST)
+	public String processForm(@ModelAttribute(value = "student") Student student) {
+		service.studentpersist(student);
+		return "success";
 	}
 
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -15,29 +15,10 @@ public class StudentService {
 	@Autowired
 	StudentRepository repo;
 
-	public List<Student> studentpersist() {
+	public void studentpersist(Student student) {
 
-		Student s1 = new Student();
-		s1.setName("rakesh");
-		s1.setCourse("java");
-		s1.setPhoneNo("7386690547");
-		repo.save(s1);
+		repo.save(student);
 
-		Student s2 = new Student();
-		s2.setName("Rajesh");
-		s2.setCourse("core java");
-		s2.setPhoneNo("8686161508");
-		repo.save(s2);
-
-		Student s3 = new Student();
-		s3.setName("sakshi");
-		s3.setCourse(".net");
-		s3.setPhoneNo("7013149523");
-		repo.save(s3);
-
-		List<Student> list = getStudentList();
-		list.stream().forEach(System.out::println);
-		return list;
 	}
 
 	public List<Student> getStudentList() {
